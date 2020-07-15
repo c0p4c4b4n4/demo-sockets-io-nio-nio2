48621614acaf48a443acbcc2642a0d3e1bf666cd
aliakh <liakh.aliaksandr@gmail.com>
2020-07-15 17:31:05 +0300

    Update

--- src/main/java/io/EchoClient.java
+++ src/main/java/io/EchoClient.java
@@ -1,15 +1,14 @@
 package io;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
 
 public class EchoClient {
 
@@ -18,14 +17,14 @@ public class EchoClient {
         String msg = "hello";
 
         OutputStream os = socket.getOutputStream();
-        OutputStreamWriter osw = new OutputStreamWriter(os);
+        OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.US_ASCII);
         PrintWriter pw = new PrintWriter(osw);
         pw.println(msg);
         System.out.println("client sent: " + msg);
         pw.flush();
 
         InputStream is = socket.getInputStream();
-        InputStreamReader isr = new InputStreamReader(is);
+        InputStreamReader isr = new InputStreamReader(is, StandardCharsets.US_ASCII);
         BufferedReader br = new BufferedReader(isr);
         System.out.println("client received: " + br.readLine());
     }
--- src/main/java/io/EchoServer.java
+++ src/main/java/io/EchoServer.java
@@ -1,15 +1,15 @@
 package io;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class EchoServer {
 
@@ -22,13 +22,13 @@ public class EchoServer {
             Socket socket = serverSocket.accept();
             try {
                 InputStream is = socket.getInputStream();
-                InputStreamReader isr = new InputStreamReader(is);
+                InputStreamReader isr = new InputStreamReader(is, StandardCharsets.US_ASCII);
                 BufferedReader br = new BufferedReader(isr);
                 String msg = br.readLine();
                 System.out.println("server received: " + msg);
 
                 OutputStream os = socket.getOutputStream();
-                OutputStreamWriter osw = new OutputStreamWriter(os);
+                OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.US_ASCII);
                 PrintWriter pw = new PrintWriter(osw);
                 pw.println(msg);
                 System.out.println("server sent: " + msg);
