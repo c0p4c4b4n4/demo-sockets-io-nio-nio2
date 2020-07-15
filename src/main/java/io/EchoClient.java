aaf6ab1fbbcd90e08c2c5a8df859fad04c4dbb7c
aliakh <liakh.aliaksandr@gmail.com>
2020-07-15 16:58:47 +0300

    Update

--- src/main/java/io/EchoClient.java
+++ src/main/java/io/EchoClient.java
@@ -11,36 +11,22 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class EchoClient
-{
-   public static void main(String[] args)
-   {
-      if (args.length != 1)
-      {
-         System.err.println("usage  : java EchoClient message");
-         System.err.println("example: java EchoClient \"This is a test.\"");
-         return;
-      }
-      try
-      {
-         Socket socket = new Socket("localhost", 9999);
-         OutputStream os = socket.getOutputStream();
-         OutputStreamWriter osw = new OutputStreamWriter(os);
-         PrintWriter pw = new PrintWriter(osw);
-         pw.println(args[0]);
-         pw.flush();
-         InputStream is = socket.getInputStream();
-         InputStreamReader isr = new InputStreamReader(is);
-         BufferedReader br = new BufferedReader(isr);
-         System.out.println(br.readLine());
-      }
-      catch (UnknownHostException uhe)
-      {
-         System.err.println("unknown host: " + uhe.getMessage());
-      }
-      catch (IOException ioe)
-      {
-         System.err.println("I/O error: " + ioe.getMessage());
-      }
-   }
+public class EchoClient {
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 9999);
+        String message = "hello";
+
+        OutputStream os = socket.getOutputStream();
+        OutputStreamWriter osw = new OutputStreamWriter(os);
+        PrintWriter pw = new PrintWriter(osw);
+        pw.println(message);
+        System.out.println("sent: " + message);
+        pw.flush();
+
+        InputStream is = socket.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        System.out.println("received: " + br.readLine());
+    }
 }
