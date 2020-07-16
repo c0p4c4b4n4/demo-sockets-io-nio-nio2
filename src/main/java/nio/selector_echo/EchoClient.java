6345f1740698f80cd9b1c4b55339193028f30110
aliakh <liakh.aliaksandr@gmail.com>
2020-07-16 14:08:16 +0300

    Update

--- src/main/java/nio/selector_echo/EchoClient.java
+++ src/main/java/nio/selector_echo/EchoClient.java
@@ -6,45 +6,21 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 
 public class EchoClient {
-    private static SocketChannel client;
-    private static ByteBuffer buffer;
-    private static EchoClient instance;
 
-    public static EchoClient start() {
-        if (instance == null)
-            instance = new EchoClient();
+    public static void main(String[] args) throws IOException {
+        SocketChannel client = SocketChannel.open(new InetSocketAddress("localhost", 5454));
 
-        return instance;
-    }
+        String msg = "hello";
 
-    public static void stop() throws IOException {
+        ByteBuffer buffer = ByteBuffer.wrap(msg.getBytes());
+
+        client.write(buffer);
+        buffer.clear();
+        client.read(buffer);
+        String response = new String(buffer.array()).trim();
+        System.out.println("response=" + response);
+        buffer.clear();
+
         client.close();
-        buffer = null;
-    }
-
-    private EchoClient() {
-        try {
-            client = SocketChannel.open(new InetSocketAddress("localhost", 5454));
-            buffer = ByteBuffer.allocate(256);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public String sendMessage(String msg) {
-        buffer = ByteBuffer.wrap(msg.getBytes());
-        String response = null;
-        try {
-            client.write(buffer);
-            buffer.clear();
-            client.read(buffer);
-            response = new String(buffer.array()).trim();
-            System.out.println("response=" + response);
-            buffer.clear();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return response;
-
     }
 }
