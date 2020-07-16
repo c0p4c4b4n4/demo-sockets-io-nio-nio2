6568d0ad2cfa0f0b223524e36b32aa4e0fc700cb
aliakh <liakh.aliaksandr@gmail.com>
2020-07-16 13:57:03 +0300

    Update

--- src/main/java/nio/selector_echo/EchoServer.java
+++ src/main/java/nio/selector_echo/EchoServer.java
@@ -1,6 +1,5 @@
 package nio.selector_echo;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -16,60 +15,48 @@ public class EchoServer {
     private static final String POISON_PILL = "POISON_PILL";
 
     public static void main(String[] args) throws IOException {
+
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.configureBlocking(false);
+
+        serverSocketChannel.bind(new InetSocketAddress("localhost", 5454));
+
         Selector selector = Selector.open();
-        ServerSocketChannel serverSocket = ServerSocketChannel.open();
-        serverSocket.bind(new InetSocketAddress("localhost", 5454));
-        serverSocket.configureBlocking(false);
-        serverSocket.register(selector, SelectionKey.OP_ACCEPT);
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
         ByteBuffer buffer = ByteBuffer.allocate(256);
 
         while (true) {
             selector.select();
-            Set<SelectionKey> selectedKeys = selector.selectedKeys();
-            Iterator<SelectionKey> iter = selectedKeys.iterator();
-            while (iter.hasNext()) {
 
-                SelectionKey key = iter.next();
+            Set<SelectionKey> selectedKeys = selector.selectedKeys();
+
+            Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
+            while (keyIterator.hasNext()) {
+                SelectionKey key = keyIterator.next();
 
                 if (key.isAcceptable()) {
-                    register(selector, serverSocket);
+                    SocketChannel socketChannel = serverSocketChannel.accept();
+                    socketChannel.configureBlocking(false);
+                    socketChannel.register(selector, SelectionKey.OP_READ);
                 }
 
                 if (key.isReadable()) {
-                    answerWithEcho(buffer, key);
+                    SocketChannel socketChannel = (SocketChannel) key.channel();
+                    socketChannel.read(buffer);
+                    if (new String(buffer.array()).trim().equals(POISON_PILL)) {
+                        socketChannel.close();
+                        System.out.println("Not accepting socketChannel messages anymore");
+                    }
+
+                    buffer.flip();
+                    socketChannel.write(buffer);
+                    buffer.clear();
                 }
-                iter.remove();
+
+                keyIterator.remove();
             }
         }
     }
 
-    private static void answerWithEcho(ByteBuffer buffer, SelectionKey key) throws IOException {
-        SocketChannel client = (SocketChannel) key.channel();
-        client.read(buffer);
-        if (new String(buffer.array()).trim().equals(POISON_PILL)) {
-            client.close();
-            System.out.println("Not accepting client messages anymore");
-        }
-
-        buffer.flip();
-        client.write(buffer);
-        buffer.clear();
-    }
-
-    private static void register(Selector selector, ServerSocketChannel serverSocket) throws IOException {
-        SocketChannel client = serverSocket.accept();
-        client.configureBlocking(false);
-        client.register(selector, SelectionKey.OP_READ);
-    }
-
-    public static Process start() throws IOException, InterruptedException {
-        String javaHome = System.getProperty("java.home");
-        String javaBin = javaHome + File.separator + "bin" + File.separator + "java";
-        String classpath = System.getProperty("java.class.path");
-        String className = EchoServer.class.getCanonicalName();
-
-        ProcessBuilder builder = new ProcessBuilder(javaBin, "-cp", classpath, className);
-
-        return builder.start();
-    }
 }
