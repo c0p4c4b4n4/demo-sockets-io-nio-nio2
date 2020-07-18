3b07b7f96cfe3a79c01b8996fe4cb36fcb435efb
s7dy1jmtxd9loas6 <s7dy1jmtxd9loas6@gmail.com>
2020-07-18 15:49:35 +0300

    Update

--- src/main/java/nio2/b/AsyncEchoServer.java
+++ src/main/java/nio2/b/AsyncEchoServer.java
@@ -10,60 +10,36 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 public class AsyncEchoServer {
-    private AsynchronousServerSocketChannel serverChannel;
-    private Future<AsynchronousSocketChannel> acceptResult;
-    private AsynchronousSocketChannel clientChannel;
 
-    public AsyncEchoServer() {
-        try {
-            serverChannel = AsynchronousServerSocketChannel.open();
-            InetSocketAddress hostAddress = new InetSocketAddress("localhost", 4999);
-            serverChannel.bind(hostAddress);
-            acceptResult = serverChannel.accept();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
+        AsynchronousServerSocketChannel serverChannel = AsynchronousServerSocketChannel.open();
+        InetSocketAddress hostAddress = new InetSocketAddress("localhost", 4999);
+        serverChannel.bind(hostAddress);
+        Future<AsynchronousSocketChannel> acceptResult = serverChannel.accept();
 
-    public void runServer() {
-        try {
-            clientChannel = acceptResult.get();
-            if ((clientChannel != null) && (clientChannel.isOpen())) {
-                while (true) {
+        AsynchronousSocketChannel clientChannel = acceptResult.get();
+        if ((clientChannel != null) && (clientChannel.isOpen())) {
+            while (true) {
+                ByteBuffer buffer = ByteBuffer.allocate(32);
+                Future<Integer> readResult = clientChannel.read(buffer);
 
-                    ByteBuffer buffer = ByteBuffer.allocate(32);
-                    Future<Integer> readResult = clientChannel.read(buffer);
+                readResult.get();
 
-                    // do some computation
+                buffer.flip();
+                String message = new String(buffer.array()).trim();
+                if (message.equals("bye")) {
+                    break;
+                }
+                buffer = ByteBuffer.wrap(new String(message).getBytes());
+                Future<Integer> writeResult = clientChannel.write(buffer);
 
-                    readResult.get();
-
-                    buffer.flip();
-                    String message = new String(buffer.array()).trim();
-                    if (message.equals("bye")) {
-                        break; // while loop
-                    }
-                    buffer = ByteBuffer.wrap(new String(message).getBytes());
-                    Future<Integer> writeResult = clientChannel.write(buffer);
-
-                    // do some computation
-                    writeResult.get();
-                    buffer.clear();
-
-                } // while()
-
-                clientChannel.close();
-                serverChannel.close();
-
+                // do some computation
+                writeResult.get();
+                buffer.clear();
             }
-        } catch (InterruptedException | ExecutionException | IOException e) {
-            e.printStackTrace();
+
+            clientChannel.close();
+            serverChannel.close();
         }
-
-    }
-
-    public static void main(String[] args) {
-        AsyncEchoServer server = new AsyncEchoServer();
-        server.runServer();
     }
 }
