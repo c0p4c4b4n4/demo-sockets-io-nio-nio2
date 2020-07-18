aef3b3ec3f7e066e6bde0d7386e1bb3939d21161
s7dy1jmtxd9loas6 <s7dy1jmtxd9loas6@gmail.com>
2020-07-18 15:56:00 +0300

    Update

--- src/main/java/io/echo/EchoClient.java
+++ src/main/java/io/echo/EchoClient.java
@@ -7,6 +7,8 @@ import java.nio.charset.StandardCharsets;
 public class EchoClient {
 
     public static void main(String[] args) throws IOException {
+        System.out.println("echo client started");
+
         Socket socket = new Socket("localhost", 9001);
         String msg = "abcdefghijklmnopqrstuvwxyz";
 
@@ -14,12 +16,14 @@ public class EchoClient {
         OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);
         PrintWriter pw = new PrintWriter(osw);
         pw.println(msg);
-        System.out.println("client sent: " + msg);
+        System.out.println("echo client sent: " + msg);
         pw.flush();
 
         InputStream is = socket.getInputStream();
         InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
         BufferedReader br = new BufferedReader(isr);
-        System.out.println("client received: " + br.readLine());
+        System.out.println("echo client received: " + br.readLine());
+
+        System.out.println("echo client finished");
     }
 }
--- src/main/java/io/echo/EchoServer.java
+++ src/main/java/io/echo/EchoServer.java
@@ -8,9 +8,8 @@ import java.nio.charset.StandardCharsets;
 public class EchoServer {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("echo server started...");
-
         ServerSocket serverSocket = new ServerSocket(9001);
+        System.out.println("echo server started: " + serverSocket);
 
         while (true) {
             Socket socket = serverSocket.accept();
@@ -19,13 +18,13 @@ public class EchoServer {
                 InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
                 BufferedReader br = new BufferedReader(isr);
                 String msg = br.readLine();
-                System.out.println("server received: " + msg);
+                System.out.println("echo server received: " + msg);
 
                 OutputStream os = socket.getOutputStream();
                 OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);
                 PrintWriter pw = new PrintWriter(osw);
                 pw.println(msg);
-                System.out.println("server sent: " + msg);
+                System.out.println("echo server sent: " + msg);
                 pw.flush();
             } finally {
                 socket.close();
