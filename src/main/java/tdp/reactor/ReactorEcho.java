0e88befc4d929be58042e79f056bedbf673843cb
aliakh <liakh.aliaksandr@gmail.com>
2020-07-31 11:07:56 +0300

    Update

--- src/main/java/tdp/reactor/ReactorEcho.java
+++ src/main/java/tdp/reactor/ReactorEcho.java
@@ -1,5 +1,6 @@
 package tdp.reactor;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
@@ -12,46 +13,34 @@ class ReactorInitiator {
 
     private static final int SERVER_PORT = 9001;
 
-    public void initiateReactiveServer(int port) throws Exception {
-
+    public void initiateReactiveServer(int port) throws IOException {
         ServerSocketChannel server = ServerSocketChannel.open();
         server.socket().bind(new InetSocketAddress(port));
         server.configureBlocking(false);
 
         Dispatcher dispatcher = new Dispatcher();
         dispatcher.registerChannel(SelectionKey.OP_ACCEPT, server);
+        dispatcher.registerEventHandler(SelectionKey.OP_ACCEPT, new AcceptEventHandler(dispatcher.getDemultiplexer()));
+        dispatcher.registerEventHandler(SelectionKey.OP_READ, new ReadEventHandler(dispatcher.getDemultiplexer()));
+        dispatcher.registerEventHandler(SelectionKey.OP_WRITE, new WriteEventHandler());
 
-        dispatcher.registerEventHandler(
-                SelectionKey.OP_ACCEPT, new AcceptEventHandler(
-                        dispatcher.getDemultiplexer()));
-
-        dispatcher.registerEventHandler(
-                SelectionKey.OP_READ, new ReadEventHandler(
-                        dispatcher.getDemultiplexer()));
-
-        dispatcher.registerEventHandler(
-                SelectionKey.OP_WRITE, new WriteEventHandler());
-
-        dispatcher.run(); // Run the dispatcher loop
+        dispatcher.run();
 
     }
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Starting NIO server at port : " +
-                SERVER_PORT);
-        new ReactorInitiator().
-                initiateReactiveServer(SERVER_PORT);
+    public static void main(String[] args) throws IOException {
+        System.out.println("Starting NIO server at port : " + SERVER_PORT);
+        new ReactorInitiator().initiateReactiveServer(SERVER_PORT);
     }
 
 }
 
 class Dispatcher {
 
-    private Map<Integer, EventHandler> registeredHandlers =
-            new ConcurrentHashMap<Integer, EventHandler>();
-    private Selector demultiplexer;
+    private final Map<Integer, EventHandler> registeredHandlers = new ConcurrentHashMap<Integer, EventHandler>();
+    private final Selector demultiplexer;
 
-    public Dispatcher() throws Exception {
+    public Dispatcher() throws IOException {
         demultiplexer = Selector.open();
     }
 
@@ -59,34 +48,29 @@ class Dispatcher {
         return demultiplexer;
     }
 
-    public void registerEventHandler(
-            int eventType, EventHandler eventHandler) {
+    public void registerEventHandler(int eventType, EventHandler eventHandler) {
         registeredHandlers.put(eventType, eventHandler);
     }
 
     // Used to register ServerSocketChannel with the
     // selector to accept incoming client connections
-    public void registerChannel(
-            int eventType, SelectableChannel channel) throws Exception {
+    public void registerChannel(int eventType, SelectableChannel channel) throws IOException {
         channel.register(demultiplexer, eventType);
     }
 
     public void run() {
         try {
-            while (true) { // Loop indefinitely
+            while (true) {
                 demultiplexer.select();
 
-                Set<SelectionKey> readyHandles =
-                        demultiplexer.selectedKeys();
-                Iterator<SelectionKey> handleIterator =
-                        readyHandles.iterator();
+                Set<SelectionKey> readyHandles = demultiplexer.selectedKeys();
+                Iterator<SelectionKey> handleIterator = readyHandles.iterator();
 
                 while (handleIterator.hasNext()) {
                     SelectionKey handle = handleIterator.next();
 
                     if (handle.isAcceptable()) {
-                        EventHandler handler =
-                                registeredHandlers.get(SelectionKey.OP_ACCEPT);
+                        EventHandler handler = registeredHandlers.get(SelectionKey.OP_ACCEPT);
                         handler.handleEvent(handle);
                         // Note : Here we don't remove this handle from
                         // selector since we want to keep listening to
@@ -94,15 +78,13 @@ class Dispatcher {
                     }
 
                     if (handle.isReadable()) {
-                        EventHandler handler =
-                                registeredHandlers.get(SelectionKey.OP_READ);
+                        EventHandler handler = registeredHandlers.get(SelectionKey.OP_READ);
                         handler.handleEvent(handle);
                         handleIterator.remove();
                     }
 
                     if (handle.isWritable()) {
-                        EventHandler handler =
-                                registeredHandlers.get(SelectionKey.OP_WRITE);
+                        EventHandler handler = registeredHandlers.get(SelectionKey.OP_WRITE);
                         handler.handleEvent(handle);
                         handleIterator.remove();
                     }
@@ -117,12 +99,12 @@ class Dispatcher {
 
 interface EventHandler {
 
-    public void handleEvent(SelectionKey handle) throws Exception;
-
+    void handleEvent(SelectionKey handle) throws Exception;
 }
 
 class AcceptEventHandler implements EventHandler {
-    private Selector demultiplexer;
+
+    private final Selector demultiplexer;
 
     public AcceptEventHandler(Selector demultiplexer) {
         this.demultiplexer = demultiplexer;
@@ -130,13 +112,11 @@ class AcceptEventHandler implements EventHandler {
 
     @Override
     public void handleEvent(SelectionKey handle) throws Exception {
-        ServerSocketChannel serverSocketChannel =
-                (ServerSocketChannel) handle.channel();
+        ServerSocketChannel serverSocketChannel = (ServerSocketChannel) handle.channel();
         SocketChannel socketChannel = serverSocketChannel.accept();
         if (socketChannel != null) {
             socketChannel.configureBlocking(false);
-            socketChannel.register(
-                    demultiplexer, SelectionKey.OP_READ);
+            socketChannel.register(demultiplexer, SelectionKey.OP_READ);
         }
     }
 
@@ -144,8 +124,8 @@ class AcceptEventHandler implements EventHandler {
 
 class ReadEventHandler implements EventHandler {
 
-    private Selector demultiplexer;
-    private ByteBuffer inputBuffer = ByteBuffer.allocate(2048);
+    private final Selector demultiplexer;
+    private final ByteBuffer inputBuffer = ByteBuffer.allocate(2048);
 
     public ReadEventHandler(Selector demultiplexer) {
         this.demultiplexer = demultiplexer;
@@ -153,8 +133,7 @@ class ReadEventHandler implements EventHandler {
 
     @Override
     public void handleEvent(SelectionKey handle) throws Exception {
-        SocketChannel socketChannel =
-                (SocketChannel) handle.channel();
+        SocketChannel socketChannel = (SocketChannel) handle.channel();
 
         socketChannel.read(inputBuffer); // Read data from client
 
@@ -164,15 +143,13 @@ class ReadEventHandler implements EventHandler {
         byte[] buffer = new byte[inputBuffer.limit()];
         inputBuffer.get(buffer);
 
-        System.out.println("Received message from client : " +
-                new String(buffer));
+        System.out.println("Received message from client : " +                new String(buffer));
         inputBuffer.flip();
         // Rewind the buffer to start reading from the beginning
         // Register the interest for writable readiness event for
         // this channel in order to echo back the message
 
-        socketChannel.register(
-                demultiplexer, SelectionKey.OP_WRITE, inputBuffer);
+        socketChannel.register(demultiplexer, SelectionKey.OP_WRITE, inputBuffer);
     }
 
 }
@@ -181,11 +158,10 @@ class WriteEventHandler implements EventHandler {
 
     @Override
     public void handleEvent(SelectionKey handle) throws Exception {
-        SocketChannel socketChannel =
-                (SocketChannel) handle.channel();
+        SocketChannel socketChannel = (SocketChannel) handle.channel();
         ByteBuffer inputBuffer = (ByteBuffer) handle.attachment();
         socketChannel.write(inputBuffer);
-        socketChannel.close(); // Close connection
+        socketChannel.close();
     }
 
 }
