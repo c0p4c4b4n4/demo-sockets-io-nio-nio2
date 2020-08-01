e0fb364a6d6a37005606898bf40f536da96d93fe
s7dy1jmtxd9loas6 <s7dy1jmtxd9loas6@gmail.com>
2020-08-01 21:57:39 +0300

    Update

--- src/main/java/io/echo/client/IoEchoBufferedReaderClient.java
+++ src/main/java/io/echo/client/IoEchoBufferedReaderClient.java
@@ -7,10 +7,12 @@ import java.nio.charset.StandardCharsets;
 public class IoEchoBufferedReaderClient {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("echo client started");
+
         String[] msgs = {"Alpha", "Bravo", "Charlie"};
         for (String msg : msgs) {
             Socket socket = new Socket("localhost", 9001);
-            System.out.println("echo client started: " + socket);
+            System.out.println("echo client connected: " + socket);
 
             OutputStream os = socket.getOutputStream();
             OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);
@@ -28,7 +30,9 @@ public class IoEchoBufferedReaderClient {
             Thread.sleep(1000);
 
             socket.close();
+            System.out.println("echo client disconnected");
         }
+
         System.out.println("echo client finished");
     }
 }
