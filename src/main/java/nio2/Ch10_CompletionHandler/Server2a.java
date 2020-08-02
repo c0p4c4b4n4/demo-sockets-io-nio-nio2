39a902c666068771483441d5ad82686ccf11de14
s7dy1jmtxd9loas6 <s7dy1jmtxd9loas6@gmail.com>
2020-08-02 21:34:05 +0300

    Update

--- src/main/java/nio2/Ch10_CompletionHandler/Server2a.java
+++ src/main/java/nio2/Ch10_CompletionHandler/Server2a.java
@@ -11,77 +11,71 @@ import java.util.concurrent.ExecutionException;
 
 public class Server2a {
 
-    public static void main(String[] args) {
-
-        final int DEFAULT_PORT = 5555;
-        final String IP = "127.0.0.1";
-
+    public static void main(String[] args) throws IOException {
         //create asynchronous server-socket channel bound to the default group
-        try (AsynchronousServerSocketChannel asynchronousServerSocketChannel = AsynchronousServerSocketChannel.open()) {
+        try (AsynchronousServerSocketChannel serverSocketChannel = AsynchronousServerSocketChannel.open()) {
 
-            if (asynchronousServerSocketChannel.isOpen()) {
+            if (serverSocketChannel.isOpen()) {
+                serverSocketChannel.setOption(StandardSocketOptions.SO_RCVBUF, 4 * 1024);
+                serverSocketChannel.setOption(StandardSocketOptions.SO_REUSEADDR, true);
 
-                //set some options
-                asynchronousServerSocketChannel.setOption(StandardSocketOptions.SO_RCVBUF, 4 * 1024);
-                asynchronousServerSocketChannel.setOption(StandardSocketOptions.SO_REUSEADDR, true);
-                //bind the server-socket channel to local address
-                asynchronousServerSocketChannel.bind(new InetSocketAddress(IP, DEFAULT_PORT));
+                serverSocketChannel.bind(new InetSocketAddress("localhost", 9001));
 
-                //display a waiting message while ... waiting clients
                 System.out.println("Waiting for connections ...");
 
-                asynchronousServerSocketChannel.accept(null, new CompletionHandler<AsynchronousSocketChannel, Void>() {
+                CompletionHandler<AsynchronousSocketChannel, Void> handler = new CompletionHandler2(serverSocketChannel);
+                serverSocketChannel.accept(null, handler);
 
-                    final ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
-
-                    @Override
-                    public void completed(AsynchronousSocketChannel result, Void attachment) {
-
-                        asynchronousServerSocketChannel.accept(null, this);
-
-                        try {
-                            System.out.println("Incoming connection from: " + result.getRemoteAddress());
-
-                            //transmitting data                  
-                            while (result.read(buffer).get() != -1) {
-
-                                buffer.flip();
-
-                                result.write(buffer).get();
-
-                                if (buffer.hasRemaining()) {
-                                    buffer.compact();
-                                } else {
-                                    buffer.clear();
-                                }
-                            }
-                        } catch (IOException | InterruptedException | ExecutionException ex) {
-                            System.err.println(ex);
-                        } finally {
-                            try {
-                                result.close();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void failed(Throwable exc, Void attachment) {
-                        asynchronousServerSocketChannel.accept(null, this);
-                        throw new UnsupportedOperationException("Cannot accept cponnections!");
-                    }
-                });
-
-                // Wait
-                System.in.read();
-
+                System.in.read(); // wait
             } else {
                 System.out.println("The asynchronous server-socket channel cannot be opened!");
             }
+        }
+    }
 
-        } catch (IOException ex) {
-            System.err.println(ex);
+    private static class CompletionHandler2 implements CompletionHandler<AsynchronousSocketChannel, Void> {
+
+        private final AsynchronousServerSocketChannel asynchronousServerSocketChannel;
+        private final ByteBuffer buffer;
+
+        public CompletionHandler2(AsynchronousServerSocketChannel asynchronousServerSocketChannel) {
+            this.asynchronousServerSocketChannel = asynchronousServerSocketChannel;
+            this.buffer = ByteBuffer.allocateDirect(1024);
+        }
+
+        @Override
+        public void completed(AsynchronousSocketChannel result, Void attachment) {
+            asynchronousServerSocketChannel.accept(null, this);
+
+            try {
+                System.out.println("Incoming connection from: " + result.getRemoteAddress());
+
+                while (result.read(buffer).get() != -1) {
+                    buffer.flip();
+
+                    result.write(buffer).get();
+
+                    if (buffer.hasRemaining()) {
+                        buffer.compact();
+                    } else {
+                        buffer.clear();
+                    }
+                }
+            } catch (IOException | InterruptedException | ExecutionException ex) {
+                System.err.println(ex);
+            } finally {
+                try {
+                    result.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        @Override
+        public void failed(Throwable exc, Void attachment) {
+            asynchronousServerSocketChannel.accept(null, this);
+            throw new UnsupportedOperationException("Cannot accept connections!");
         }
     }
 }
