9f36f709e49c8038bbf54141bd9e6f7f8922b35b
s7dy1jmtxd9loas6 <s7dy1jmtxd9loas6@gmail.com>
2020-08-02 18:00:59 +0300

    Update

--- src/main/java/nio2/b/AsyncEchoClient.java
+++ src/main/java/nio2/b/AsyncEchoClient.java
@@ -11,77 +11,29 @@ import java.util.concurrent.Future;
 
 public class AsyncEchoClient {
 
-    private AsynchronousSocketChannel client;
-    private Future<Void> future;
-    private static AsyncEchoClient instance;
+    public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
+        AsynchronousSocketChannel client = AsynchronousSocketChannel.open();
 
-    private AsyncEchoClient() {
-        try {
-            client = AsynchronousSocketChannel.open();
-            InetSocketAddress hostAddress = new InetSocketAddress("localhost", 4999);
-            future = client.connect(hostAddress);
-            start();
+        Future<Void> connectResult = client.connect(new InetSocketAddress("localhost", 9001));
+        connectResult.get();
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+        String message = "hello";
+        System.out.println("request to server:" + message);
 
-    public static AsyncEchoClient getInstance() {
-        if (instance == null)
-            instance = new AsyncEchoClient();
-        return instance;
-    }
+        byte[] bytes = message.getBytes();
+        ByteBuffer buffer = ByteBuffer.wrap(bytes);
 
-    private void start() {
-        try {
-            future.get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public String sendMessage(String message) {
-        byte[] byteMsg = message.getBytes();
-        ByteBuffer buffer = ByteBuffer.wrap(byteMsg);
         Future<Integer> writeResult = client.write(buffer);
+        writeResult.get();
 
-        try {
-            writeResult.get();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         buffer.flip();
         Future<Integer> readResult = client.read(buffer);
-        try {
-            readResult.get();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        String echo = new String(buffer.array()).trim();
+        readResult.get();
+
+        String response = new String(buffer.array()).trim();
         buffer.clear();
-        return echo;
-    }
+        System.out.println("response from server: " + response);
 
-    public void stop() {
-        try {
-            client.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        client.close();
     }
-
-    public static void main(String[] args) throws Exception {
-        AsyncEchoClient client = AsyncEchoClient.getInstance();
-        client.start();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line;
-        System.out.println("Message to server:");
-        while ((line = br.readLine()) != null) {
-            String response = client.sendMessage(line);
-            System.out.println("response from server: " + response);
-            System.out.println("Message to server:");
-        }
-    }
-
 }
