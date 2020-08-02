4c1ad9daa56a96daa3a322ab1fccc68edd4a869f
s7dy1jmtxd9loas6 <s7dy1jmtxd9loas6@gmail.com>
2020-08-02 12:47:11 +0300

    Update

---
+++ src/main/java/nio2/client/client/Attachment.java
@@ -0,0 +1,13 @@
+package nio2.client.client;
+
+import java.nio.ByteBuffer;
+
+import java.nio.channels.AsynchronousSocketChannel;
+
+public class Attachment 
+{
+   public AsynchronousSocketChannel channel;
+   public boolean isReadMode;
+   public ByteBuffer buffer;
+   public Thread mainThd;
+}
