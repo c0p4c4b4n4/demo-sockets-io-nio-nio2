c0edd8580406f6cea163b7d77728bfda315ce9ec
aliakh <liakh.aliaksandr@gmail.com>
2020-08-03 10:32:13 +0300

    Update

---
+++ src/main/java/demo/common/Demo.java
@@ -0,0 +1,9 @@
+package demo.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Demo {
+
+    protected static final Logger logger = LoggerFactory.getLogger(Demo.class);
+}
