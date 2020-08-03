fd2f7ed3919edfd6f5b780a39b061ffeb5af901a
aliakh <liakh.aliaksandr@gmail.com>
2020-08-03 15:39:40 +0300

    Update

---
+++ src/main/java/demo/nio2/completion_handler/echo/ProactorInitiator.java
@@ -0,0 +1,30 @@
+package demo.nio2.completion_handler.echo;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.AsynchronousServerSocketChannel;
+
+public class ProactorInitiator {
+
+    private static final int SERVER_PORT = 7000;
+
+    public void initiateProactiveServer(int port) throws IOException {
+        AsynchronousServerSocketChannel listener = AsynchronousServerSocketChannel.open().bind(new InetSocketAddress(port));
+        AcceptCompletionHandler acceptCompletionHandler = new AcceptCompletionHandler(listener);
+
+        SessionState state = new SessionState();
+        listener.accept(state, acceptCompletionHandler);
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Starting Proactor NIO2 echo server at port: " + SERVER_PORT);
+        new ProactorInitiator().initiateProactiveServer(SERVER_PORT);
+
+        // sleep indefinitely since otherwise the JVM would terminate
+        while (true) {
+            Thread.sleep(Long.MAX_VALUE);
+        }
+    }
+}
+
+
